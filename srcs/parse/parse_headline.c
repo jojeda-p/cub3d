7eaf84dc0e4ef3b750cb9e1dd5edbff47da0b384
jojeda-p <josunom@gmail.com>
2026-06-04 12:44:16 +0200

    pocos cambios de healine

--- srcs/parse/parse_headline.c
+++ srcs/parse/parse_headline.c
@@ -6,18 +6,53 @@
 /*   By: jojeda-p <jojeda-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 17:22:25 by jojeda-p          #+#    #+#             */
-/*   Updated: 2026/06/03 18:31:53 by jojeda-p         ###   ########.fr       */
+/*   Updated: 2026/06/04 12:40:21 by jojeda-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+/*pone el path en la estructura final y comprueba que no se repita la textura*/
+static int	get_texture_path(t_game *g, char *s, int **found, char *path)
+{
+	if (s[0] == 'N' && found[0] == 0)
+	{
+		g->tex[0].path = path;
+		*found[0] = 1;
+		return (0);
+	}
+	else if (s[0] == 'S' && found[0] == 0)
+	{
+		g->tex[1].path = path;
+		*found[1] = 1;
+		return (0);
+	}
+	else if (s[0] == 'E' && found[0] == 0)
+	{
+		g->tex[2].path = path;
+		*found[2] = 1;
+		return (0);
+	}
+	else if (s[0] == 'W' && found[0] == 0)
+	{
+		g->tex[3].path = path;
+		*found[3] = 1;
+		return (0);
+	}
+	return (1);
+}
+
 int	parse_texture(char **matrix, t_game *g, char *s)
 {
 	int		i;
 	char	*path;
+	int		found[4];
 
 	i = 0;
+	found[0] = 0;
+	found[1] = 0;
+	found[2] = 0;
+	found[3] = 0;
 	while(i < 4)
 	{
 		if (matrix[i][0] == s[0] && matrix[i][1] == s[1])
@@ -25,15 +60,7 @@ int	parse_texture(char **matrix, t_game *g, char *s)
 			path = ft_strdup(matrix[i] + 3);
 			if (parse_permisions(path) != 0)
 				return (1);
-			if (s[0] == 'N')
-				g->tex[0].path = path;
-			else if (s[0] == 'S')
-				g->tex[1].path = path;
-			else if (s[0] == 'E')
-				g->tex[2].path = path;
-			else if (s[0] == 'W')
-				g->tex[3].path = path;
-			return (0);
+			return (get_texture_path(g, s, &found, path));
 		}
 		i++;
 	}
